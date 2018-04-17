cf2694d1f26e60fbdb4e1ffa74d265009ed73e43
stephengrider <ste.grider@gmail.com>
2018-04-17 16:33:22 -0600

    oninput

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <input />
+    <input v-on:input="onInput" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchBar'
+  name: 'SearchBar',
+  methods: {
+    onInput: function(event) {
+      console.log(event.target.value);
+    }
+  }
 };
 </script>
 
