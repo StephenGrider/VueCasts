e50e6db785ee50d5f25162c62e50736bd5e05def
stephengrider <ste.grider@gmail.com>
2018-04-17 16:30:45 -0600

    nest searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    Hi there!
+    <SearchBar></SearchBar>
   </div>
 </template>
 
 <script>
+import SearchBar from './components/SearchBar';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SearchBar
+  }
 };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <input />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar'
+};
+</script>
+
+<style>
+</style>
