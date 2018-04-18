67fd2b4b3086dc8c86dca3a0a4339b55d0ad90d2
stephengrider <ste.grider@gmail.com>
2018-04-18 11:30:15 -0600

    prop validation

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,14 @@
 <template>
   <ul>
     VideoList
+    {{ videos.length }}
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'VideoList'
+  name: 'VideoList',
+  props: ['videos']
 };
 </script>
 
