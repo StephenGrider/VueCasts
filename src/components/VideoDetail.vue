0bbaca5cf286a32743a4c2d04b0dc83ede6a3a89
stephengrider <ste.grider@gmail.com>
2018-04-24 15:23:23 -0600

    craft youtube url

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,8 @@
 <template>
   <div v-if="video">
+    <div>
+      <iframe :src="videoUrl" />
+    </div>
     <div class="details">
       <h4>{{ video.snippet.title }}</h4>
       <p>{{ video.snippet.description }}</p>
@@ -10,7 +13,13 @@
 <script>
 export default {
   name: 'VideoDetail',
-  props: ['video']
+  props: ['video'],
+  computed: {
+    videoUrl() {
+      const { videoId } = this.video.id;
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+  }
 };
 </script>
 
