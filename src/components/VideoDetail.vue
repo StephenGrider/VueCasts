4a940c180841ad673379ecd2fef0597f16042c41
stephengrider <ste.grider@gmail.com>
2018-04-24 15:14:43 -0600

    vid title

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div v-if="video">
-    {{ video.snippet.title }}
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
   </div>
 </template>
 
@@ -10,3 +13,12 @@ export default {
   props: ['video']
 };
 </script>
+
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
