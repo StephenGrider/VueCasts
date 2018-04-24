20f10248d48ab067be31c991b8b508b811a07a4b
stephengrider <ste.grider@gmail.com>
2018-04-24 14:16:57 -0600

    styling

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,7 +1,9 @@
 <template>
-  <li class="list-group-item">
-    <img :src="thumbnailUrl" />
-    {{ video.snippet.title }}
+  <li class="list-group-item media">
+    <img class="mr-3" :src="thumbnailUrl" />
+    <div class="media-body">
+      {{ video.snippet.title }}
+    </div>
   </li>
 </template>
 
@@ -16,3 +18,14 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+li {
+  display: flex;
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: #eee;
+}
+</style>
