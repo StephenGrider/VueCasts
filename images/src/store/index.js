51bcfb9bef8188c21ba358a365f7f49bacb04c45
stephengrider <ste.grider@gmail.com>
2018-04-26 17:32:27 -0600

    wire in auth module

--- images/src/store/index.js
+++ images/src/store/index.js
@@ -1,8 +1,11 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
+import auth from './modules/auth';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {}
+  modules: {
+    auth
+  }
 });
