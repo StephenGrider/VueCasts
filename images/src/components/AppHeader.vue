b1d8625949873827da3c2a731161bbc109363814
stephengrider <ste.grider@gmail.com>
2018-04-26 17:49:04 -0600

    initial req

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -3,11 +3,20 @@
     <a href="/" class="active item">
       Image Storage
     </a>
+
+    <div class="right menu">
+      <a href="#" class="ui item" @click="login">
+        Login
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  methods: mapActions(['login'])
 };
 </script>
