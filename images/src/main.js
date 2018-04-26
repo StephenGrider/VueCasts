958436d15630a95e20a52d16c3180a3adeac5296
stephengrider <ste.grider@gmail.com>
2018-04-26 16:33:50 -0600

    auth

--- images/src/main.js
+++ images/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App';
+import store from './store';
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');
---
+++ images/src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {}
+});
