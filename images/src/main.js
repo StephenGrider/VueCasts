05b98c1ead3b0ddc302ef74d26736563b9d04b06
stephengrider <ste.grider@gmail.com>
2018-04-27 14:55:37 -0600

    additional route config

--- images/src/main.js
+++ images/src/main.js
@@ -3,12 +3,18 @@ import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
 import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList';
+import UploadForm from './components/UploadForm';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   mode: 'history',
-  routes: [{ path: '/oauth2/callback', component: AuthHandler }]
+  routes: [
+    { path: '/', component: ImageList },
+    { path: '/upload', component: UploadForm },
+    { path: '/oauth2/callback', component: AuthHandler }
+  ]
 });
 
 new Vue({
