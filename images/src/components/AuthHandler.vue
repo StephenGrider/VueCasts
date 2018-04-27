6413336eadec1629ce6dcbd9f5bfeca4db8a6996
stephengrider <ste.grider@gmail.com>
2018-04-27 12:02:33 -0600

    call finalizelogin

--- images/src/components/AuthHandler.vue
+++ images/src/components/AuthHandler.vue
@@ -5,10 +5,13 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'AuthHandler',
+  methods: mapActions(['finalizeLogin']),
   created() {
-    //
+    this.finalizeLogin(window.location.hash);
   }
 };
 </script>
