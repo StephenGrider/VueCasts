d78ead1dbc755eece53ce5cd4a6312bb1898d486
stephengrider <ste.grider@gmail.com>
2018-04-27 13:03:51 -0600

    mapgetters

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <a href="/" class="active item">
       Image Storage
     </a>
-
+    {{ isLoggedIn }}
     <div class="right menu">
       <a href="#" class="ui item" @click="login">
         Login
@@ -13,10 +13,11 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'AppHeader',
+  computed: mapGetters(['isLoggedIn']),
   methods: mapActions(['login'])
 };
 </script>
