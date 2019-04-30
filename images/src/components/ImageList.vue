c4f1b30d40cf8685cb7a8f0a29244101ea0f0957
stephengrider <ste.grider@gmail.com>
2019-04-30 16:35:04 -0600

    add css grid

--- images/src/components/ImageList.vue
+++ images/src/components/ImageList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="image-container">
     <img v-for="image in allImages" :src="image.link"  />
   </div>
 </template>
@@ -16,3 +16,14 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+img {
+  max-width: 100%;
+  padding: 5px;
+}
+</style>
