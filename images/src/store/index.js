da2304f09e4b9cac9e9e4b89d5d9144749ca7346
stephengrider <ste.grider@gmail.com>
2020-01-27 16:12:02 -0600

    image response

--- images/src/store/index.js
+++ images/src/store/index.js
@@ -1,11 +1,13 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
 import auth from './modules/auth';
+import images from './modules/images';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    images
   }
 });
