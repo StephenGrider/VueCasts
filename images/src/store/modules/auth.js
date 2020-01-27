34a77a3bb0581af716ef4f025c6bb655190982ed
stephengrider <ste.grider@gmail.com>
2020-01-27 13:17:30 -0600

    persist login

--- images/src/store/modules/auth.js
+++ images/src/store/modules/auth.js
@@ -2,7 +2,7 @@ import api from '../../api/imgur';
 import qs from 'qs';
 
 const state = {
-  token: null
+  token: window.localStorage.getItem('imgur_token')
 };
 
 const getters = {
@@ -17,6 +17,7 @@ const actions = {
     const query = qs.parse(hash.replace('#', ''));
 
     commit('setToken', query.access_token);
+    window.localStorage.setItem('imgur_token', query.access_token);
   },
   logout: ({ commit }) => {
     commit('setToken', null);
