574c8f09fae6b3ba100be02d207d5fef5523898a
stephengrider <ste.grider@gmail.com>
2020-01-27 16:21:05 -0600

    commit images to state

--- images/src/store/modules/images.js
+++ images/src/store/modules/images.js
@@ -9,10 +9,10 @@ const getters = {
 };
 
 const actions = {
-  async fetchImages({ rootState }) {
+  async fetchImages({ rootState, commit }) {
     const { token } = rootState.auth;
     const response = await api.fetchImages(token);
-    console.log(response);
+    commit('setImages', response.data.data);
   }
 };
 
