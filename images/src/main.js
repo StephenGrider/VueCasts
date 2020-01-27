2871c3860e873e57a5ca7c57f149ca83ac0a23df
stephengrider <ste.grider@gmail.com>
2020-01-27 11:39:31 -0600

    use browser router

--- images/src/main.js
+++ images/src/main.js
@@ -7,6 +7,7 @@ import AuthHandler from './components/AuthHandler';
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  mode: 'history',
   routes: [{ path: '/oauth2/callback', component: AuthHandler }]
 });
 
