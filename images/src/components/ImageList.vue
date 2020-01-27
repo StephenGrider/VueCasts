8f61393c60d790588734754798217cc1f50cbbab
stephengrider <ste.grider@gmail.com>
2020-01-27 14:55:37 -0600

    additional route config

---
+++ images/src/components/ImageList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Image List
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageList'
+};
+</script>
---
+++ images/src/components/UploadForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Upload form
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UploadForm'
+};
+</script>
