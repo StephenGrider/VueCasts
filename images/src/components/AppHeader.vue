6c24ccddf0f0c51171c3eb8c21aa6073a90de284
stephengrider <ste.grider@gmail.com>
2020-01-27 15:24:07 -0600

    images module implementation

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="ui secondary pointing menu">
-    <a href="/" class="active item">
+    <router-link to="/" class="active item">
       Image Storage
-    </a>
+    </router-link>
 
     <div class="right menu">
       <div v-if="isLoggedIn" class="horizontal">
-        <a class="item">Galleries</a>
-        <a class="item">Upload</a>
+        <router-link to="/" class="item">Galleries</router-link>
+        <router-link to="/upload" class="item">Upload</router-link>
         <a class="item" @click="logout">Logout</a>
       </div>
 
