da1ed8092c272a8380566f675861c6903ca54422
stephengrider <ste.grider@gmail.com>
2020-01-27 15:34:22 -0600

    fetching images

--- images/src/api/imgur.js
+++ images/src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = 'fbb60590d660043';
 const ROOT_URL = 'https://api.imgur.com';
@@ -13,5 +14,12 @@ export default {
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
       querystring
     )}`;
+  },
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 };
