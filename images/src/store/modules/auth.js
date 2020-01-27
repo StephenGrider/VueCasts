fdcf01500683e58dce003c44ee85e0602999d1db
stephengrider <ste.grider@gmail.com>
2020-01-27 12:10:08 -0600

    get token

--- images/src/store/modules/auth.js
+++ images/src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur';
+import qs from 'qs';
 
 const state = {
   token: null
@@ -11,6 +12,11 @@ const getters = {
 const actions = {
   login: () => {
     api.login();
+  },
+  finalizeLogin({ commit }, hash) {
+    const query = qs.parse(hash.replace('#', ''));
+
+    commit('setToken', query.access_token);
   },
   logout: ({ commit }) => {
     commit('setToken', null);
