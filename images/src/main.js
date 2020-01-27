7a87a2d972bbd276cb12706391cc4e04a8470978
stephengrider <ste.grider@gmail.com>
2020-01-27 13:39:12 -0600

    navigation

--- images/src/main.js
+++ images/src/main.js
@@ -6,7 +6,7 @@ import AuthHandler from './components/AuthHandler';
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
+export const router = new VueRouter({
   mode: 'history',
   routes: [{ path: '/oauth2/callback', component: AuthHandler }]
 });
--- images/src/store/modules/auth.js
+++ images/src/store/modules/auth.js
@@ -1,5 +1,6 @@
 import api from '../../api/imgur';
 import qs from 'qs';
+import { router } from '../../main';
 
 const state = {
   token: window.localStorage.getItem('imgur_token')
@@ -18,6 +19,7 @@ const actions = {
 
     commit('setToken', query.access_token);
     window.localStorage.setItem('imgur_token', query.access_token);
+    router.push('/');
   },
   logout: ({ commit }) => {
     commit('setToken', null);
