86e9d8036569dac41d96d5a68cbe4269a50d4dcd
stephengrider <ste.grider@gmail.com>
2020-01-30 15:30:43 -0600

    upload styling

--- images/src/components/UploadForm.vue
+++ images/src/components/UploadForm.vue
@@ -16,5 +16,18 @@ export default {
   height: 30vh;
   border: 2px dashed black;
   border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.dropper:hover {
+  background-color: #eee;
+}
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
 }
 </style>
